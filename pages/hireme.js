7c2beae930a5b61a61ba731f86370f3cccc7fbed
amit-lekhak <amitlekhak1@gmail.com>
2021-04-30 17:16:33 +0545

    next portfolio commit

---
+++ pages/hireme.js
@@ -0,0 +1,12 @@
+import Layout from "../components/Layout";
+
+const HireMe = () => (
+  <Layout title="Hire Me">
+    <p>
+      You can hire me at{" "}
+      <a href="mailto:amitlekhak1@gmail.com">amitlekhak1@gmail.com</a>
+    </p>
+  </Layout>
+);
+
+export default HireMe;
